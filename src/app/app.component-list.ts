32609d440307654fc603e71f072723fa7a0aea9d
Wojciech <wojciech.najda.sg@gmail.com>
2018-02-13 18:14:16 +0100

    Created header component.

--- src/app/app.component-list.ts
+++ src/app/app.component-list.ts
@@ -1,7 +1,9 @@
 import { NavigationComponent } from '../app/navigation/navigation.component';
 import { HttpComponent } from './http/http.component';
+import { HeaderComponent } from './header/header.component';
 
 export const AppComponentList = [
   NavigationComponent,
-  HttpComponent
+  HttpComponent,
+  HeaderComponent
 ];
