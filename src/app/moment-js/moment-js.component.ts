428f90ec94433711381a4caa793b85afc0c2aca4
Wojciech <wojciech.najda.sg@gmail.com>
2018-03-12 19:52:08 +0100

    Adding time to the selected date.

--- src/app/moment-js/moment-js.component.ts
+++ src/app/moment-js/moment-js.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'moment-js',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moment-js.component.scss']
 })
 export class MomentJsComponent implements OnInit {
+  firstDate: number;
+  days: number = 0;
+  weeks: number = 0;
+  months: number = 0;
+  years: number = 0;
+
+  result: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  checking() {
+    this.result = moment(this.firstDate).add({
+      days: this.days,
+      weeks: this.weeks,
+      months: this.months,
+      years: this.years
+    }).format('YYYY-MM-DD');
   }
 
 }
