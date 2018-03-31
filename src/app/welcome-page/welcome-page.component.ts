2f3205df8d8a280e43ca32f198175a0457250794
Wojciech Najda <wojciech.najda@inwedo.com>
2018-03-31 13:50:25 +0200

    Style for footer

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
+  isPhoneNumber: boolean = false;
+  isMail: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showPhoneNumber() {
+    this.isMail = false
+    this.isPhoneNumber = true;
+    setTimeout(() => this.isPhoneNumber = false, 10000)
+  }
+
+  showMail() {
+    this.isPhoneNumber = false;
+    this.isMail = true;
+    setTimeout(() => this.isMail = false, 10000)
   }
 
 }
